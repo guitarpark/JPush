2a62455cae685674bcca3fdebc1f222d1361243b
guitarpark <guitarpark@gmail.com>
2018-01-24 14:37:20 +0800

    极光推送

---
+++ JPush.Bindings.iOS/Structs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace JPush.Bindings.iOS
+{
+	[Native]
+public enum JPAuthorizationOptions : int
+{
+	None = 0,
+	Badge = (1 << 0),
+	Sound = (1 << 1),
+	Alert = (1 << 2)
+}
+}
